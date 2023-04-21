7428c26f7ec32ebba1010617d2fd3a89459a38d2
Wilmer Cantillo <will.canti2697@gmail.com>
2023-04-21 12:49:38 -0500

    log request to be in test

--- packages/web-tests/tests/index.spec.ts
+++ packages/web-tests/tests/index.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('fe should stablish connection with be', async ({ page }) => {
+  console.log(await fetch('http://localhost:4000/health').then((r) => r.json()))
   await page.goto('http://localhost:3000/')
 
   const apiResponse = page.getByText('"status": "ok"')
