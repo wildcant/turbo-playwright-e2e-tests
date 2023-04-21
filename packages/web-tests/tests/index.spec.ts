866eae9c46c8d6da545bf49065d41039e84064b1
Wilmer Cantillo <will.canti2697@gmail.com>
2023-04-21 13:07:16 -0500

    split check

--- packages/web-tests/tests/index.spec.ts
+++ packages/web-tests/tests/index.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from '@playwright/test'
 
 test('fe should stablish connection with be', async ({ page, request }) => {
-  console.log(await request.get(`http://127.0.0.1:4000/health`))
+  console.log(await (await request.get(`http://127.0.0.1:4000/health`)).json())
 
   await page.goto('/')
 
-  const apiResponse = page.getByText('"status": "ok"')
+  const apiResponse1 = page.getByText('status')
+  const apiResponse2 = page.getByText('ok')
 
-  await expect(apiResponse).toBeVisible()
+  await expect(apiResponse1).toBeVisible()
+  await expect(apiResponse2).toBeVisible()
 })
 
 // TODO: Add docker compose to github actions
